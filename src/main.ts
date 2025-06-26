dfdac4da8c5600e4f663c07ae4bb3c503e46c88b
Ruslan Galiev <ghaliev@gmail.com>
2025-06-26 13:14:02 +0500

    Заменил ф-ю запуска приложения

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+bootstrap().catch((err) => {
+	console.error('Ошибка запуска приложения:', err);
+	process.exit(1);
+});
